df404ec4cef91860ceb263e8e36a1e97135e42d4
William Cadenas <williamcadenas21@gmail.com>
2018-09-20 18:58:01 -0500

    [feat]: made a x in the monitor

--- Lab3/lab3.vhd
+++ Lab3/lab3.vhd
@@ -5,43 +5,43 @@ use ieee.numeric_std.all;
 
 ENTITY lab3 IS
 PORT(
-CLOCK_24: IN STD_LOGIC_VECTOR(1 downto 0);
+CLOCK_50: IN STD_LOGIC;
+VGA_CLK: out std_logic;
 VGA_HS,VGA_VS:OUT STD_LOGIC;
-SW: STD_LOGIC_VECTOR(1 downto 0);
-KEY: STD_LOGIC_VECTOR(3 DOWNTO 0);
-VGA_R,VGA_B,VGA_G: OUT STD_LOGIC_VECTOR(3 downto 0)
+VGA_R,VGA_B,VGA_G: OUT STD_LOGIC_VECTOR(7 downto 0)
 );
 END lab3;
 
 
 ARCHITECTURE MAIN OF lab3 IS
-SIGNAL VGACLK,RESET:STD_LOGIC;
+SIGNAL VGACLK,RESET: STD_LOGIC:='0';
+
  COMPONENT SYNC IS
  PORT(
 	CLK: IN STD_LOGIC;
 	HSYNC: OUT STD_LOGIC;
 	VSYNC: OUT STD_LOGIC;
-	R: OUT STD_LOGIC_VECTOR(3 downto 0);
-	G: OUT STD_LOGIC_VECTOR(3 downto 0);
-	B: OUT STD_LOGIC_VECTOR(3 downto 0);
-	KEYS: IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-   S: IN STD_LOGIC_VECTOR(1 downto 0)
+	R: OUT STD_LOGIC_VECTOR(7 downto 0);
+	G: OUT STD_LOGIC_VECTOR(7 downto 0);
+	B: OUT STD_LOGIC_VECTOR(7 downto 0)
 	);
 END COMPONENT SYNC;
-
-
-
-    component pll is
+----------------------------------------------
+    component PLL is
         port (
-            clkout_clk : out std_logic;        -- clk
-            clkin_clk  : in  std_logic := 'X'; -- clk
-            rst_reset  : in  std_logic := 'X'  -- reset
+            clk_in_clk : in std_logic := 'X'; 
+				reset_reset  : in  std_logic := 'X';	
+            clk_out_clk  : out  std_logic 
+           
         );
-	 END COMPONENT pll;
+	 END COMPONENT PLL;
+-----------------------------------------------	 
  BEGIN
  
- C: pll PORT MAP (VGACLK,CLOCK_24(0),RESET);
- C1: SYNC PORT MAP(VGACLK,VGA_HS,VGA_VS,VGA_R,VGA_G,VGA_B,KEY,SW);
+ 
+ C1: SYNC PORT MAP(VGACLK,VGA_HS,VGA_VS,VGA_R,VGA_G,VGA_B);
+ C2: PLL PORT MAP (CLOCK_50,RESET,VGACLK);
+ VGA_CLK <= VGACLK;
  
  END MAIN;
  
