7e2278c60bb4bae8bfb888fedf802e1845bc47d1
DavidCuentasMar <davidandrescuentasmar@gmail.com>
2018-09-24 11:50:18 -0500

    commit with some errors

--- Lab3/vhdl2.vhd
+++ Lab3/vhdl2.vhd
@@ -1,6 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+USE work.my.all;
 
 ENTITY SYNC IS
 PORT(
@@ -25,22 +26,29 @@ SIGNAL HPOS: INTEGER RANGE 0 TO 1688:=0;
 --- visible area 1024, front porch 1, sync pulse 3, back porch 38
 --- whole line 1066
 SIGNAL VPOS: INTEGER RANGE 0 TO 1066:=0;
-BEGIN
 
+SIGNAL SQ_X1,SQ_Y1: INTEGER RANGE 0 TO 1688:=0;
+SIGNAL RGB: STD_LOGIC_VECTOR(3 DOWNTO 0);
+SIGNAL DRAW: STD_LOGIC;
+BEGIN
+SQ_X1<=1048;
+SQ_Y1<=554;
+SQ(HPOS,VPOS,SQ_X1,SQ_Y1,RGB,DRAW);
  PROCESS(CLK)
  BEGIN
-IF(CLK'EVENT AND CLK='1')THEN
+IF(CLK'EVENT AND CLK='1') then
 
-	IF(HPOS=1048 OR VPOS=554)THEN
-		R<=(OTHERS=>'1');
-		G<=(OTHERS=>'0');
-		B<=(OTHERS=>'0');
+   --the middle is ((BP,FP,Sync) + visible area/2)
+
+	IF(DRAW='1')THEN
+		R<=RGB;
+		G<=RGB;
+		B<=RGB;
 		ELSE
 		R<=(OTHERS=>'0');
 		G<=(OTHERS=>'0');
 		B<=(OTHERS=>'0');
 	END IF;
-
 
 	IF(HPOS < 1688)THEN
 	HPOS<=HPOS+1;
@@ -65,10 +73,11 @@ IF(CLK'EVENT AND CLK='1')THEN
 		VSYNC<='1';
 	END IF;
 	
-	IF((HPOS>0AND HPOS<408)OR(VPOS>0 AND VPOS< 42))THEN
+	IF((HPOS>0 AND HPOS<408)OR(VPOS>0 AND VPOS< 42))THEN
 		R<=(OTHERS=>'0');
 		G<=(OTHERS=>'0');
 		B<=(OTHERS=>'0');
+		
 	END IF;
 END IF;	
 END PROCESS;
