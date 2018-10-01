cac949f5e0b35f73d4fdce5d2a2e5bd3e6d9dd6e
DavidCuentasMar <davidandrescuentasmar@gmail.com>
2018-10-01 11:11:56 -0500

    ps2keyboard integration

---
+++ Lab3/Lab2/Lab2.vhd.bak
@@ -0,0 +1,43 @@
+library ieee ;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
+
+-----------------------------------------------------
+
+entity lab2 is
+port(	
+	
+	--inputs
+	clock:		in std_logic;
+	reset:		in std_logic;
+	velocity:	in std_logic;
+	
+	--outputs
+
+);
+end lab2;
+
+-----------------------------------------------------
+
+architecture FSM of lab2 is
+
+	signal clockTimer: integer:= 0; --2hz 
+	signal count: integer := 0;
+
+begin
+
+-- this process verify the number of the count
+process(clock) 
+	
+	begin
+	
+
+end process;						  
+
+process(number) begin
+ 
+
+end process;
+
+end FSM;
--- Lab3/lab3.vhd
+++ Lab3/lab3.vhd
@@ -8,6 +8,10 @@ PORT(
 CLOCK_50: IN STD_LOGIC;
 VGA_CLK: out std_logic;
 VGA_HS,VGA_VS:OUT STD_LOGIC;
+ps2_clk: 	in  std_logic;
+ps2_data: 	in  std_logic;
+display_right : out  std_logic_vector(0 to 6) := "1111111";
+display_left : out  std_logic_vector(0 to 6) := "1111111";
 VGA_R,VGA_B,VGA_G: OUT STD_LOGIC_VECTOR(7 downto 0)
 );
 END lab3;
@@ -35,12 +39,28 @@ END COMPONENT SYNC;
            
         );
 	 END COMPONENT PLL;
------------------------------------------------	 
- BEGIN
+-----------------------------------------------
+component Lab2 is
+port(	
+
+--inputs
+	clock:		in std_logic;
+	ps2_clk:		in  std_logic;--clock signal from PS2 keyboard
+	ps2_data: 	in  std_logic;
+
+	display_right : out  std_logic_vector(0 to 6) := "1111111";
+	display_left : out  std_logic_vector(0 to 6):= "1111111";
+	led_vector : out  std_logic_vector(7 downto 0)
+
+);
+END COMPONENT Lab2;
+BEGIN
+ -----------------------------------------------
  
  C2: PLL PORT MAP (CLOCK_50,RESET,VGACLK);
  VGA_CLK <= VGACLK;
  C1: SYNC PORT MAP(VGACLK,VGA_HS,VGA_VS,VGA_R,VGA_G,VGA_B);
+ C3: Lab2 PORT MAP(CLOCK_50,ps2_clk,ps2_data,display_right,display_left);
  
  
  
